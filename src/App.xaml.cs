d56a1cd1529e3071be2e379967bdffc90610b104
Ma-TA <ma-ta@users.noreply.github.com>
2020-11-02 20:48:39 +0100

    Add files via upload

---
+++ src/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace BudizMail
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ src/OAplikaciWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace BudizMail
+{
+    /// <summary>
+    /// Interakční logika pro OAplikaciWindow.xaml
+    /// </summary>
+    public partial class OAplikaciWindow : Window
+    {
+        public OAplikaciWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
