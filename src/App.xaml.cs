7360fc6804d703ff61c48b1bff90c48fff64bf7a
Martin Tábor <ma-ta@users.noreply.github.com>
2020-12-29 18:08:37 +0100

    verze 0.2.3
    .NET SDK 5.0.101
    MSVS 2019 16.8.3

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -10,7 +10,7 @@ namespace BudizMail
         public static string NAZEV = "Budiž Mail";
         public static string AUTOR = "Martin TÁBOR";
         public static string LICENCE = "CC BY-SA 4.0";
-        public static string VERZE = "0.2.2  |  20.B14";
+        public static string VERZE = "0.2.3  |  20.C30";
         public static string GITHUB = "github.com/ma-ta/budiz-mail";
     }
 }
--- stara-verze/README.md
+++
@@ -1,6 +0,0 @@
-# SendMail
-> Odeslání e-mailu (.NET Framework 4.8, WPF, VS Community 2019 v16.3.2)
-
-Binárka pro Win7–Win10 – [https://github.com/ma-ta/budiz-mail/blob/main/stara-verze/PosliMailWPF.exe)
-
-![Aplikace PošliMail](https://github.com/ma-ta/budiz-mail/blob/main/stara-verze/PosliMail.png)
